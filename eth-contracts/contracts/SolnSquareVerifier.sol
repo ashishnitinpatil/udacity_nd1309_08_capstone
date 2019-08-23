2b8896c04cc4ff65df4ed8222d678790dbdfeb7f
Ashish Patil <ashishnitinpatil@gmail.com>
2019-08-24 01:44:03 +0400

    +implemented SolnSquareVerifier with tests

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,62 +1,66 @@
 pragma solidity 0.5.11;
 
 import {DecentralandERC721Token} from "./ERC721Mintable.sol";
-
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import {SquareVerifier} from "./SquareVerifier.sol";
 
 
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is DecentralandERC721Token {
+    // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+    SquareVerifier public verifierContract;
+
+    constructor(address verifierAddress) public{
+        verifierContract = SquareVerifier(verifierAddress);
+    }
+
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint256 tokenId;
+        address to;
+    }
+
+    // TODO define an array of the above struct
+    Solution[] submittedSolutions;
+
+    // TODO define a mapping to store unique solutions submitted
+    mapping(bytes32 => Solution) uniqueSolutions;
+
+    // TODO Create an event to emit when a solution is added
+    event SolutionAdded(
+        address indexed to,
+        uint256 indexed tokenId,
+        bytes32 indexed key
+    );
+
+    // TODO Create a function to add the solutions to the array and emit the event
+    function _addSolution(address _to, uint256 _tokenId, bytes32 _key)
+        internal
+    {
+        Solution memory _soln = Solution({tokenId : _tokenId, to : _to});
+        submittedSolutions.push(_soln);
+        uniqueSolutions[_key] = _soln;
+        emit SolutionAdded(_to, _tokenId, _key);
+    }
+
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSupply
+    function mintToken(
+        address to,
+        uint256 tokenId,
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory input
+    )
+        public
+        whenNotPaused
+    {
+        bytes32 key = keccak256(abi.encodePacked(a, b, c, input));
+        require(uniqueSolutions[key].to == address(0), "Solution is already used");
+        require(verifierContract.verifyTx(a, b, c, input), "Solution is incorrect");
+        _addSolution(to, tokenId, key);
+        super.mint(to, tokenId);
+    }
 }
-
-
-// TODO define a solutions struct that can hold an index & an address
-
-
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -2,7 +2,10 @@
 const SquareVerifier = artifacts.require("SquareVerifier");
 const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
 
+
 module.exports = function(deployer) {
-    deployer.deploy(SquareVerifier);
-    deployer.deploy(SolnSquareVerifier);
+    deployer.deploy(SquareVerifier)
+    .then(() => {
+        return deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
+    });
 };
